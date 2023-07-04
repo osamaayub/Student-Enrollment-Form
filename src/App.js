f0ffe4294ade1dc66e9bce4bd1e0f74b20554f0c
Osama ayub <ayubosama416@gmail.com>
2023-07-05 02:05:37 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import EnrollmentForm from './components/EnrollmentForm';
+
+const App = () => {
+  return (
+    <div id='system'>
+      <EnrollmentForm/>
+    </div>
+  )
+}
+
+export default App
